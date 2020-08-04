5a0b8c13a859e2f8c969d60f30fd7f59b7c24cdd
behnaz-fs <43137011+behnaz-fs@users.noreply.github.com>
2020-08-05 01:05:14 +0430

    update pdf files

--- src/templates/static/legal/regulation.jsx
+++ src/templates/static/legal/regulation.jsx
@@ -144,7 +144,7 @@ const Regulation = () => {
                             center
                             border='border-dark-gray'
                             image='images/pages/regulation/pdf-icon.svg'
-                            href={it.url_for('/download/BIEL_Pillar_3_Report_20200421.pdf')}
+                            href={it.url_for('/download/DIEL_Pillar_3_Report_20200421.pdf')}
                             target='_blank'
                             text={it.L('Pillar 3 disclosure report')}
                         />
@@ -169,9 +169,9 @@ const Regulation = () => {
                                 border='border-dark-gray'
                                 className='margin-top-17 align-start'
                                 image='images/pages/regulation/pdf-icon.svg'
-                                href={it.url_for('/download/BIEL-RTS28-for-2018.pdf')}
+                                href={it.url_for('/download/DIEL-RTS28-for-2018.pdf')}
                                 target='_blank'
-                                text={it.L('BIEL RTS28 for 2018')}
+                                text={it.L('DIEL RTS28 for 2018')}
                             />
                         </div>
                         <div className='gr-6 gr-12-m'>
@@ -181,9 +181,9 @@ const Regulation = () => {
                                 border='border-dark-gray'
                                 className='margin-top-17 align-start'
                                 image='images/pages/regulation/pdf-icon.svg'
-                                href={it.url_for('/download/BIEL-RTS28-for-2019.pdf')}
+                                href={it.url_for('/download/DIEL-RTS28-for-2019.pdf')}
                                 target='_blank'
-                                text={it.L('BIEL RTS28 for 2019')}
+                                text={it.L('DIEL RTS28 for 2019')}
                             />
                         </div>
                     </div>
